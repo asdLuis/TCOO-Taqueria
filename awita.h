3b2153c54982ea1d0fa68eaae38842cd0e69050f
zLuis <a01705457@itesm.mx>
2022-11-29 00:01:12 -0600

    changed UML

--- awita.h
+++ awita.h
@@ -10,22 +10,22 @@ class Awita : public Articulo
 private:
     std::string flavour; // name
     int size; // 1-2-3
-    bool hasIce; // true - false
+    bool has_ice; // true - false
 
 public:
     // Constructor
     Awita(){};
-    Awita(std::string f, int s, bool hi) : flavour(f), size(s), hasIce(hi) {};
+    Awita(std::string f, int s, bool hi) : flavour(f), size(s), has_ice(hi) {};
 
     // Setters
     void set_flavour(std::string );
     void set_size(int );
-    void set_hasIce(bool );
+    void set_has_ice(bool );
 
     // Getters
     std::string get_flavour();
     int get_size();
-    bool get_hasIce();
+    bool get_has_ice();
 
     std::string set_as_string();
 };
@@ -38,8 +38,8 @@ void Awita::set_flavour(std::string f) {
 void Awita::set_size(int s) {
     size = s;
 }
-void Awita::set_hasIce(bool hi) {
-    hasIce = hi;
+void Awita::set_has_ice(bool hi) {
+    has_ice = hi;
 }
 
 std::string Awita::get_flavour() {
@@ -48,12 +48,12 @@ std::string Awita::get_flavour() {
 int Awita::get_size() {
     return size;
 }
-bool Awita::get_hasIce() {
-    return hasIce;
+bool Awita::get_has_ice() {
+    return has_ice;
 }
 std::string Awita::set_as_string() {
     std::stringstream ss;
-    ss << "Awita of " << flavour << ", " << size << " L, " << (hasIce ? "with" : "without") << " ice.";
+    ss << "Awita of " << flavour << ", " << size << " L, " << (has_ice ? "with" : "without") << " ice.";
     return ss.str();
 }
 
